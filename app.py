10ca0c168fcf3cfffec896345fd2682e94ff6612
rofohe <110810471+rofohe@users.noreply.github.com>
2025-12-28 12:15:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from langdetect import detect, LangDetectException
 import gspread
 from google.oauth2.service_account import Credentials
 
+pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
+
 # --------------------------------------------------
 # Page config
 # --------------------------------------------------
